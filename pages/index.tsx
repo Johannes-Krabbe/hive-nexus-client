8918aa077014bb744b0b9e3f22fbec5f7099c0a3
alex <57222349+spacjalex@users.noreply.github.com>
2023-02-17 12:10:13 +0100

    add overlay placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
 
 import Button from 'components/button/button'
 import { Sun } from '../components/index/sun/sun'
 import { Hero } from '../components/core/hero/hero';
 import { PostList } from '../components/index/feed/post-list'
+import { CreatePost } from '../components/index/feed/create-post'
 
 import styles from '../components/core/layout/index.module.scss'
 
@@ -11,6 +13,8 @@ import { postData } from '../lib/data/data'
 
 const Home: NextPage = () => {
   const headline = 'hivenexus'
+
+  const [overlayIsShowing, setOverlayIsShowing] = useState(false)
 
   return (
     <div className={styles.Container}>
@@ -24,9 +28,27 @@ const Home: NextPage = () => {
         action={'button'}
         variant={'primary'}
         text={'Create post'}
-        onClick={() => {console.log('Creating Post')}}
+        onClick={() => setOverlayIsShowing(!overlayIsShowing)}
         />
       <PostList posts={postData} />
+      {overlayIsShowing && (
+          <div className={styles.OverlaySection}>
+            <div
+              className={styles.OverlayBackground}
+              onClick={() => setOverlayIsShowing(!overlayIsShowing)}
+            >
+              <div className={styles.Close}>
+                <a href="#" className={styles.CloseButton}></a>
+              </div>
+              <div
+                className={styles.OverlayContent}
+                onClick={(e) => e.stopPropagation()}
+              >
+                <CreatePost id={'1234'} createdAt={"12.12.1991"} author={'spacjalex'} title={'title title'} content={'hello helllo content sdf sdf sdf '} />
+              </div>
+            </div>
+          </div>
+        )}
     </div>
   )
 }
