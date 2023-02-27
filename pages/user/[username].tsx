c96ad42f52dd881ec7997b882c469f7408a598b9
Johannes Krabbe <Johannes@Krabbe.net>
2023-02-27 10:31:25 +0100

    cors other error

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -6,9 +6,10 @@ import { Sun } from "components/core/layout/sun/sun";
 import { LoadingSpinner } from "components/core/layout/loading/loading-spinner";
 
 import styles from "components/core/layout/index.module.scss";
+import { request } from "utils/context";
 
 const UserPage: NextPage = () => {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState<any>(null);
   const [isLoading, setLoading] = useState(true);
 
   const router = useRouter();
@@ -17,30 +18,30 @@ const UserPage: NextPage = () => {
 
   useEffect(() => {
     if (!router.isReady) return;
+    async function fetchData() {
+      const res = await request.get(`/user/one?username=${username}`);
 
-    fetch(`https://dummyjson.com/users/${username}`)
-    // fetch(`http://localhost:3001/user?username=${username}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(`userData received: ${JSON.stringify(data)}`)
-        setUserData(data);
-        setLoading(false);
-      })
-      .catch(err => {
-        console.log('request failed', err)
-      })
-  }, [router]);
+      console.log("qqwfpqw", res);
+      console.log(`userData received: ${JSON.stringify(res)}`);
+      setUserData(res.data.data);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, [router, username]);
 
   if (isLoading) return <LoadingSpinner />;
-  if (!userData) return <p>No User Data</p>;
-
-  return (
-    <div className={styles.Container}>
-      <Sun color={"sun-peach"} />
-      <p>Username: {userData.username}</p>
-      <p>Email: {userData.email}</p>
-    </div>
-  );
+  if (!userData) {
+    return <p>No User Data</p>;
+  } else {
+    return (
+      <div className={styles.Container}>
+        <Sun color={"sun-peach"} />
+        <p>Username: {userData.username}</p>
+        <p>Email: {userData.email}</p>
+      </div>
+    );
+  }
 };
 
 export default UserPage;
